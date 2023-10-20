2e3a238624efd8e2c016c1d55e7e29f697a283bd
MuvMotors <147949307+MarcoResh@users.noreply.github.com>
2023-10-20 07:49:40 -0400

    Add files via upload
    Sketch to test stereo from DFPlayer. Includes a wav file testing left and right channels.

---
+++ 1-Wire_Multilple_Temps/1-Wire_Multilple_Temps.ino
@@ -0,0 +1,58 @@
+ /* 1-Wire_Multiple_Temps.ino
+ *  Sensors wired in parallel to IO-12 on the 
+ *  SB_T41_7ext or SB_T41_4on_2ext boards. Same IO pins.
+ */
+ 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 12 on the Arduino
+#define ONE_WIRE_BUS 12
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+int deviceCount = 0;
+float tempC;
+
+void setup(void)
+{
+  sensors.begin();  // Start up the library
+  sensors.setResolution(10); // resolution 0.125C, takes minimum of 188mS
+  Serial.begin(9600);
+
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+}
+
+void loop(void)
+{
+  // Send command to all the sensors for temperature conversion
+  sensors.requestTemperatures();
+
+  // Display temperature from each sensor
+  for (int i = 0;  i < deviceCount;  i++)
+  {
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(" : ");
+    tempC = sensors.getTempCByIndex(i);
+    Serial.print(tempC);
+    Serial.print( "\xC2\xB0"); // shows degree symbol
+    Serial.print("C  |  ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    Serial.print( "\xC2\xB0"); // shows degree symbol
+    Serial.println("F");
+  }
+
+  Serial.println("");
+  delay(1000);
+}
